e881e2a4567e521bc5178f8d68f4070b8f840f3f
JoseRibelles <joseantonioribellesgarcia@elpuig.xeill.net>
2026-04-21 10:48:15 +0200

    Remove server proxy configuration
    Removed server proxy configuration from Vite config.

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      }
-    }
-  }
+  plugins: [vue()]
 })
